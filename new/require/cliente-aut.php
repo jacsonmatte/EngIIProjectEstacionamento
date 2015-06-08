b8f491e8e13cf31f02eb1dc20602e06676e710c2
adpeiter <aristosdp@gmail.com>
2015-06-08 12:50:37 -0300

    retirada verificação de sessão iniciada

--- new/require/cliente-aut.php
+++ new/require/cliente-aut.php
@@ -1,6 +1,5 @@
 <?php
-	if (session_status() == PHP_SESSION_NONE)
-		session_start();
+	session_start();
 	if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 2) {
 		echo "Você não está a autorizado a ver esta página!";
 		die();
