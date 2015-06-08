dba365b26fd527af71148a73b1f0db335ca7ded4
adpeiter <aristosdp@gmail.com>
2015-06-08 12:50:20 -0300

    retirada verificação de sessão iniciada

--- new/require/adm-aut.php
+++ new/require/adm-aut.php
@@ -1,6 +1,5 @@
 <?
-	if (session_status() == PHP_SESSION_NONE)
-		session_start();
+	session_start();
 	if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 1) {
 		echo("Você não está a autorizado a ver esta página	!");
 		die();
