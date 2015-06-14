627a4f918ca980fb7758eacc66f0fd82b6e0dfa5
jacsonmatte <jacsonmatte@gmail.com>
2015-06-14 17:05:30 -0300

    Ajuste Login
    Ajuste na section do login
    Jacson e Wagner

--- new/validation.php
+++ new/validation.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if(isset($_POST["login"]) and isset($_POST["senha"])){
 
    $mysql['username'] = addslashes($_POST['login']);
@@ -6,9 +7,13 @@ if(isset($_POST["login"]) and isset($_POST["senha"])){
    require "bd/conectBd.php";
    $res = validaLogin($mysql['username'], $mysql['password']);
    if($res > 0 && $mysql['username'] == "admin"){
-	   header("Location:adm/home.php");
+	   $_SESSION['user_type'] = 1;
+		$_SESSION['username'] = $mysql['username'];
+	  header("Location: adm/home.php");
    }else{
 	   if($res > 0){
+		   $_SESSION['user_type'] = 2;
+		   $_SESSION['username'] = $mysql['username'];
 		    header("Location:cliente/home.php");
 	   }
    }
