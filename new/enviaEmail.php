1ae3e3cc32b6f64f0f430a7f2a57ff56d7b51adb
wagnerfrana94 <wagnerfrana@hotmail.com>
2015-06-16 01:12:00 -0300

    Alteração Função que envia Email de redefinição de senha

--- new/enviaEmail.php
+++ new/enviaEmail.php
@@ -5,39 +5,65 @@ require_once("PHPMailer/class.phpmailer.php");
 // Inicia a classe PHPMailer
 $mail = new PHPMailer();
 
+$remetente = "controlparking01@gmail.com"; // Aqui vai do email remetente o qual aparecerá no e-mail enviado;
+$senharemetente = "controlparking"; // senha da conta de e-mail do remetente;
+
+
 // Define os dados do servidor e tipo de conexão
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 $mail->IsSMTP(); // Define que a mensagem será SMTP
-$mail->Host = "smtp.dominio.net"; // Endereço do servidor SMTP
-//$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
-//$mail->Username = 'seumail@dominio.net'; // Usuário do servidor SMTP
-//$mail->Password = 'senha'; // Senha do servidor SMTP
+$mail->SMTPSecure  =  "tls" ;
+$mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP (mudar o dominio EX: smtp.dominio.com)
+$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
+$mail->SMTP_PORT = 465;  
+$mail->Username = "$remetente"; // Usuário do servidor SMTP
+$mail->Password = "$senharemetente"; // Senha do servidor SMTP
 
 // Define o remetente
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->From = "wagnerfrana94@gmail.com"; // Seu e-mail
-$mail->FromName = "Control Parking"; // Seu nome
 
+$mail->From = "$remetente"; // Seu e-mail
+$mail->FromName = "Control Parking"; // Seu nome
 // Define os destinatário(s)
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->AddAddress('$email', '$nome');
-$mail->AddAddress('$email');
+
+$mail->AddAddress("$email");
 //$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
 //$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
-
 // Define os dados técnicos da Mensagem
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-//$mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
+
+$mail->CharSet = 'utf-8'; // Charset da mensagem (opcional)
+
 
 // Define a mensagem (Texto e Assunto)
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->Subject  = "Envio da nova senha Control Parking"; // Assunto da mensagem
-$mail->Body = "Control Parking : Nova Senha" .'$nova_senha'."<b>HTML</b>!  :)";
-$mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n :)";
+$quebra = "<br>"; 
+
+$mail->Subject  = "Redefinição Senha Control Parking"; // Assunto da mensagem
+$mail->Body = "Redefinição Senha Control Parking";
+$mail->Body .= $quebra; 
+$mail->Body .= "Senha enviada em " . date("d/m/Y");
+$mail->Body .= $quebra; 
+$mail->Body .= $quebra; 
+$mail->Body .= "Seguem seus dados abaixo: ";
+$mail->Body .= $quebra;
+$mail->Body .= $quebra;
+$mail->Body .= "Seu login é: " . $login ;
+$mail->Body .= $quebra;
+$mail->Body .= "Sua nova senha é: " . $nova_senha ;
+$mail->Body .= $quebra;
+$mail->Body .= $quebra; 
+$mail->Body .= "E-mail automático! Não responda este e-mail";
+
+//$mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n ";
 
 // Define os anexos (opcional)
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 //$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
 
 // Envia o e-mail
@@ -46,8 +72,6 @@ $enviado = $mail->Send();
 // Limpa os destinatários e os anexos
 $mail->ClearAllRecipients();
 $mail->ClearAttachments();
-
-// Exibe uma mensagem de resultado
 if ($enviado) {
  	echo "<script language=javascript>alert( 'Email Enviado Com Sucesso Com a Nova Senha !' );</script>";
 	echo("<script type='text/javascript'>location.href='index.html';</script>");
@@ -56,3 +80,5 @@ if ($enviado) {
 	echo("<script type='text/javascript'>location.href='index.html';</script>");
   	echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
 }
+
+?>
