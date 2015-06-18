03f639f1545c3a77221694e7e399a497a65d823a
wagnerfrana94 <wagnerfrana@hotmail.com>
2015-06-18 14:02:57 -0300

    Finalização páginas de cadastro de planos e vagas
    Wagner e Geovani

--- new/adm/cadastrar_plano.php
+++ new/adm/cadastrar_plano.php
@@ -17,35 +17,108 @@
 		require '../require/menu-adm.html';
 		require '../require/menu-2-content-1.html';
 	?> 
+	<script type="text/javascript">
+		
+
+		function validaCampo(){
+			if(document.form_plano.nome.value==""){
+				alert("O Campo nome do plano é obrigatório!");
+				document.getElementById('txtNome').focus();
+				return false;
+				
+			}else if(document.form_plano.valor.value==""){
+				alert("O Campo valor do plano é obrigatório!");
+				document.getElementById('txtValor').focus();
+				return false;	
+
+			}else if(document.form_plano.qt_horas.value==""){
+				alert("O Campo quantidade de horas é obrigatório!");
+				document.getElementById('txtQuantidadeHoras').focus();
+				return false;
+						
+			}else if(document.form_plano.valor_hr_excedente.value==""){
+				alert("O Campo valor da hora excedente é obrigatório!");
+				document.getElementById('txtValorHoraExcedente').focus();
+				return false;
+			}
+		}		
+	</script>
 	<h3>Cadastro de Planos</h3>
-	<form role='form' class='text-center'>
+	<form role='form' class='text-center' method="post" action=" " name="form_plano" onsubmit="return validaCampo(); return false;"> 
 		<div class='form-group col-sm-6'>
 			<label for='txtNome'>Nome:</label>
-			<input type='text' class='form-control' id='txtNome' placeholder='Ex: Plano executivo 1, Plano passeio...'>
+			<input type='text' class='form-control' id='txtNome' name="nome" placeholder='Ex: Plano executivo 1, Plano passeio...'>
 		</div>
 		<div class='form-group col-sm-6'>
 			<label for='txtValor'>Valor:</label>
-			<input type='text' class='form-control' id='txtValor' placeholder='Digite o valor do plano'>
+			<input type='text' class='form-control' id='txtValor' name="valor" placeholder='Digite o valor do plano'>
 		</div>
 		<div class='form-group col-sm-6'>
 			<label for='txtQuantidadeHoras'>Quantidade de horas:</label>
-			<input type='text' class='form-control' id='txtQuantidadeHoras' placeholder='Digite a quantidade de horas'>
+			<input type='text' class='form-control' id='txtQuantidadeHoras' name="qt_horas" placeholder='Digite a quantidade de horas'>
 		</div>
 		<div class='form-group col-sm-6'>
 			<label for='txtValorHoraExcedente'>Hora excedente:</label>
-			<input type='text' class='form-control' id='txtValorHoraExcedente' placeholder='Digite o valor da hora excedente'>
+			<input type='text' class='form-control' id='txtValorHoraExcedente' name="valor_hr_excedente" placeholder='Digite o valor da hora excedente'>
 		</div>
 		<div class='form-group col-sm-12'>
 			<label for='txtDescricao'>Descri&ccedil;&atilde;o:</label>
-			<textarea class='form-control' id='txtDescricao' placeholder='Digite alguma descri&ccedil;&atilde;o para o plano (opcional)'></textarea>
+			<textarea class='form-control' id='txtDescricao' name="descricao" placeholder='Digite alguma descri&ccedil;&atilde;o para o plano (opcional)'></textarea>
 		</div>
 		<div class='form-group col-sm-12 text-right'>
 			<span id='spnErroSalvarPlano'></span>
 			<input type='button' id='btnCancelar' class='btn btn-danger min-border-white' value='Cancelar' /> &nbsp;
-			<input type='button' id='btnSalvar' class='btn cmd-item' value='Salvar' />
+			<input type='submit' id='btnSalvar' class='btn cmd-item' name="btnSalvar" value='Salvar' />
 		</div>
 	</form>
+	<script type="text/javascript">
+		
+		$( "#btnCancelar").click(function() {
+			$(location).attr('href', 'home.php');
+		});
+	</script>
+	
 	<?php
 		require '../require/content-2-footer.html';
+	
+
+		if(isset($_POST["btnSalvar"])){
+				
+				require_once("../bd/conectBd.php");
+				$conexao=dbConnect("localhost","root","");
+
+				$nome=addslashes($_POST["nome"]);
+	 			$valor=addslashes($_POST["valor"]);
+	 			$qt_horas=addslashes($_POST["qt_horas"]);
+	 			$valor_hr_excedente=addslashes($_POST["valor_hr_excedente"]);
+	 			$descricao=addslashes($_POST["descricao"]);
+	 			
+				$sql="INSERT INTO plano values('NULL','$nome','$valor','$qt_horas', '$valor_hr_excedente', '$descricao')";
+				$pega=dbConsulta($sql,'estacionamento', $conexao);
+				
+				if(!$pega){
+					echo "<script language=javascript>alert( 'Falha ao cadastrar o Plano !!!' );</script>";
+				}else{
+					echo "<script language=javascript>alert( 'Novo Plano Cadastrado com Sucesso !!!' );</script>";
+				}
+				mysql_close($conexao);
+		}				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	?>
 </html>
--- new/adm/cadastrar_vaga.php
+++ new/adm/cadastrar_vaga.php
@@ -8,6 +8,24 @@
 			require '../require/meta.html';
 			require '../require/js-base.html';
 		?>
+
+	<script type="text/javascript">
+		
+
+		function validaCampo(){
+			if(document.form_vaga.cod_vaga.value==""){
+				alert("O Campo Numero da Vaga é obrigatório!");
+				document.getElementById('txtCodigo').focus();
+				return false;
+				
+			}else if(document.form_vaga.tipo_vaga.value==""){
+				alert("O Campo tipo da vaga é obrigatório!");
+				document.getElementById('sltTipo').focus();
+				return false;	
+
+			}
+		}		
+	</script>	
 	</head>
 	<?php
 		require '../require/menu-1.html';
@@ -16,30 +34,66 @@
 		require '../require/menu-2-content-1.html';
 	?> 
 	<h3>Cadastro de Vagas</h3>
-		<form role='form' class='text-center'>
+	<form role='form' class='text-center' name="form_vaga" action="cadastrar_vaga.php" method="POST" onsubmit="return validaCampo(); return false;">
 		<div class='form-group col-sm-6'>
 			<label for='txtValor'>C&oacute;digo:</label>
-			<input type='text' class='form-control' id='txtCodigo' placeholder='Digite o c&oacute;digo da vaga'>
+			<input type='number' class='form-control' id='txtCodigo' name="cod_vaga" placeholder='Digite o c&oacute;digo da vaga'>
 		</div>
 		<div class='form-group col-sm-6'>
 			<label for='sltTipo'>Tipo de vaga:</label>
-			<select class='form-control' id='sltTipo'>
-				<option>Carro</option>
-				<option>Moto</option>
-				<option>Utilit&aacute;rio</option>
+			<select class='form-control' id='sltTipo' name="tipo_vaga">
+				<option value="Carro">Carro</option>
+				<option value="Moto">Moto</option>
+				<option value="Utilitário">Utilit&aacute;rio</option>
 			</select>
 		</div>
 		<div class='form-group col-sm-12'>
 			<label for='txtDescricao'>Descri&ccedil;&atilde;o:</label>
-			<textarea class='form-control' id='txtDescricao' placeholder='Digite alguma descri&ccedil;&atilde;o para o plano (opcional)'></textarea>
+			<textarea class='form-control' id='txtDescricao' name="descricao_vaga" placeholder='Digite alguma descri&ccedil;&atilde;o para o plano (opcional)'></textarea>
 		</div>
 		<div class='form-group col-sm-12 text-right'>
 			<span id='spnErroSalvarVaga'></span>
 			<input type='button' id='btnCancelar' class='btn btn-danger min-border-white' value='Cancelar' /> &nbsp;
-			<input type='button' id='btnSalvar' class='btn cmd-item' value='Salvar' />
+			<input type='submit' id='btnSalvar' class='btn cmd-item' value='Salvar' name="btnSalvar"/>
 		</div>
 	</form>
+
+	<script type="text/javascript">
+		
+		$( "#btnCancelar").click(function() {
+			$(location).attr('href', 'home.php');
+		});
+	</script>	
+	
+	
 	<?php
 		require '../require/content-2-footer.html';
+	
+
+
+		if(isset($_POST["btnSalvar"])){
+				
+				require_once("../bd/conectBd.php");
+				$conexao=dbConnect("localhost","root","");
+
+				$cod_vaga=addslashes($_POST["cod_vaga"]);
+	 			$tipo_vaga=addslashes($_POST["tipo_vaga"]);
+	 			$descricao_vaga=addslashes($_POST["descricao_vaga"]);
+	 			
+				$sql="INSERT INTO vaga values('NULL','$cod_vaga','$descricao_vaga','$tipo_vaga')";
+				$pega=dbConsulta($sql,'estacionamento', $conexao);
+				
+				if(!$pega){
+					echo "<script language=javascript>alert( 'Falha ao cadastrar Vaga !!!' );</script>";
+				}else{
+					echo "<script language=javascript>alert( 'Nova Vaga Cadastrada com Sucesso !!!' );</script>";
+				}
+				mysql_close($conexao);
+		}				
+
+
+
+
+
 	?>
 </html>
