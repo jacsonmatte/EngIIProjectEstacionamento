d1ae247c8e909f2a45467f7b7056fe89c2760e91
adpeiter <aristosdp@gmail.com>
2015-06-22 15:42:06 -0300

    correção
    descomentadas linhas que incluem as datas, o tipo e a situação no sql das reservas

--- new/bd/reserva.php
+++ new/bd/reserva.php
@@ -38,7 +38,7 @@ function efetuarReserva($vaga, $dataHoraInicial, $dataHoraFinal, $cliente) {
 	else return -1;
 	
 	$res = dbConsulta("SELECT token FROM estacionamento WHERE id_estacionamento = '$idReserva'", "estacionamento", $con);
-	//$con->close;
+	
 	return mysql_fetch_array($res)[0];
 	
 }
@@ -47,18 +47,17 @@ function buscarReservas($cliente, $dataInicial, $dataFinal, $tipoVaga, $situacao
 
 	$sql = "SELECT e.id_estacionamento codigo, e.dh_entrada entrada, e.dh_saida saida, v.nro_vaga vaga, e.token token, e.status status FROM estacionamento e JOIN vaga v ON e.vaga_id_vaga = v.id_vaga WHERE cliente_id_cliente = '$cliente'";
 	
-/*	if ($dataInicial != '' && $dataFinal != '')
+	if ($dataInicial != '' && $dataFinal != '')
 		$sql = $sql . " AND (dh_entrada BETWEEN '$dataInicial' AND '$dataFinal' OR dh_saida BETWEEN '$dataInicial' AND '$dataFinal')";
 	if ($tipoVaga > 0)
 		$sql = $sql . " AND (SELECT tipo FROM vaga WHERE id_vaga = e.vaga_id_vaga) = '$tipoVaga'";
 	if ($situacao > 0)
 		$sql = $sql . " AND status = '$situacao'";
-*/
 
 	$con = dbConnect("localhost", "root", "");
 	$res = dbConsulta($sql, "estacionamento", $con);
 	
 	return $res;
-	//$con->close;
+
 }
 ?>
