63715833076bf6b1ba46823fe158dc3966458feb
Matheus "Salko" Junges <matheus.bsj@gmail.com>
2015-06-22 21:55:52 -0300

    Página de mensalidades do ADM e função de conexão com o banco
    Tarefa executada por Edimar Junior e Matheus Junges, com auxílio de
    Aristides, Chrystian, Daiano e Patric.

--- new/adm/mensalidades.php
+++ new/adm/mensalidades.php
@@ -1,4 +1,7 @@
-<?php require '../require/adm-aut.php'; ?>
+<?php 
+	require '../require/adm-aut.php'; 
+	require "../bd/conectBd.php";
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,34 +18,79 @@
 		require '../require/menu-2-content-1.html';
 	?> 
 	<h3>Mensalidades</h3>
-		<form role='form' class='text-center'>
-		<div class='form-group col-sm-3 text-left'>
+		<form role='form' class='text-center' method='POST' action='mensalidades.php'>
+			<div class='form-group col-sm-3 text-left'>
 			<label for='sltPesquisaMes'>Mês:</label>
-			<select class='form-control' id='sltPesquisaMes'>
-				<?php // função ?>
+			<select class='form-control' id='sltPesquisaMes' name='mes'>
+				<option value="Janeiro">Janeiro</option>
+				<option value="Fevereiro">Fevereiro</option>
+				<option value="Março">Março</option>
+				<option value="Abril">Abril</option>
+				<option value="Maio">Maio</option>
+				<option value="Junho">Junho</option>
+				<option value="Julho">Julho</option>
+				<option value="Agosto">Agosto</option>
+				<option value="Setembro">Setembro</option>
+				<option value="Outubro">Outubro</option>
+				<option value="Novembro">Novembro</option>
+				<option value="Dezembro">Dezembro</option>
 			</select>
 		</div>
+		
 		<div class='form-group col-sm-3 text-left'>
 			<label for='sltPesquisaAno'>Ano:</label>
-			<select class='form-control' id='sltPesquisaAno'>
-				<option>2014</option>
-				<option>2015</option>
-				<option>2016</option>
+			<select class='form-control' id='sltPesquisaAno' name='ano'>
+				<option value="2014">2014</option>
+				<option value="2015">2015</option>
+				<option value="2016">2016</option>
 			</select>
 		</div>
 		<div class='form-group col-sm-6 text-left'>
 			<label for='txtPesquisaNomeCliente'>Cliente:</label>
-			<input type='text' class='form-control' id='txtPesquisaNomeCliente' placeholder='Digite o nome (ou parte) do cliente' />
+			<input type='text' class='form-control' id='txtPesquisaNomeCliente' name="nome" value="Nome"/>
 		</div>
 		<div class='form-group col-sm-12 text-right'>
 			<span id='spnErroPesquisarMensalidades'></span>
-			<input type='button' id='btnPesquisar' class='btn cmd-item' value='Pesquisar' />
+			<input type='submit' id='btnPesquisar' class='btn cmd-item' value='Pesquisar' />
 		</div>
 		<div id='divResultadoPesquisaMensalidades' class='form-group col-sm-12 text-center'>
-			<strong>Nenhuma mensalidade encontrada ou pesquisada por enquanto</strong>
+		
+				
+			<?php
+				if(isset($_POST['mes']) && isset($_POST['ano']) && isset($_POST['nome'])){
+
+
+					$dados = buscaMensalidade($_POST['mes'], $_POST['ano'], $_POST['nome']);
+					
+					if (mysql_num_rows($dados) > 0) {	
+					
+						echo '<table width="100%">';
+						echo '<thead><tr>';
+						echo '<th>Nome</th>';
+						echo '<th>Mês</th>';
+						echo '<th>Ano</th>';
+						echo '<th>Gasto</th>';
+						echo '</thead></tr>';
+						
+						echo '<tbody>';
+						while ($dados1 = mysql_fetch_array($dados)){
+					    	echo '<tr>';
+					    	echo '<td>'.$dados1['nome'].'</td>';
+					    	echo '<td>'.$dados1['mes'].'</td>';
+					    	echo '<td>'.$dados1['ano'].'</td>';
+					    	echo '<td>'. number_format($dados1['soma'], 2) .'</td>';
+					    	echo '</tr>';
+						}
+						echo '</tbody></table>';
+					}
+					else {
+						echo '<strong>Nenhuma mensalidade encontrada</strong>';
+					}
+				}
+			?>
 		</div>
 	</form>
-	<?php
-		require '../require/content-2-footer.html';
-	?>
+
 </html>
+<?php require '../require/content-2-footer.html'; ?>
+
