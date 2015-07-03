1d6cc4f08cbfdd3907ccf3e8a4734e1a793bc60b
Aristides D. Peiter <aristosdp@gmail.com>
2015-07-03 00:58:19 -0300

    dump do banco com as correções
    alterações no arquivo new/alterações
    página efetuar reserva ainda tem pendências
    (dei commit mesmo assim pra n ficar com muita coisa pendente)

--- dataBase/estacionamento_com_Inserts.sql
+++ dataBase/estacionamento_com_Inserts.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 16-Jun-2015 às 22:03
--- Versão do servidor: 5.6.21
--- PHP Version: 5.5.19
+-- Generation Time: 03-Jul-2015 às 05:55
+-- Versão do servidor: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,47 +21,31 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema estacionamento
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `estacionamento` ;
-
--- -----------------------------------------------------
--- Schema estacionamento
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `estacionamento` ;
-USE `estacionamento` ;
-
-
 
 --
 -- Estrutura da tabela `cliente`
 --
 
-CREATE TABLE IF NOT EXISTS `estacionamento`.`cliente` (
-`id_cliente` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id_cliente` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL COMMENT 'tempo em de estacionamento',
-  `cpf_cnpj` varchar(30) NOT NULL COMMENT 'o custo em relação ao tempo',
+  `cpf_cnpj` varchar(14) NOT NULL,
   `email` varchar(45) DEFAULT NULL,
   `logradouro` varchar(45) DEFAULT NULL,
-  `nro` int(11) DEFAULT NULL,
-  `cep` varchar(45) DEFAULT NULL,
+  `nro` int(6) DEFAULT NULL,
+  `cep` varchar(8) DEFAULT NULL,
   `bairro` varchar(45) DEFAULT NULL,
   `cidade` varchar(45) DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
   `telefone` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
 
 --
 -- Extraindo dados da tabela `cliente`
 --
 
-INSERT INTO `cliente` (`id_cliente`, `nome`, `cpf_cnpj`, `email`, `logradouro`, `nro`, `cep`, `bairro`, `cidade`, `estado`, `telefone`) VALUES
-(1, 'Pedro', '05015283609', 'pedro@gmail.com', 'Rua dos indios', 23, '89814021', 'São Pedro', 'Chapecó', 'Santa Catarina', '(49) 99560123');
+INSERT INTO `cliente` (`id_cliente`, `nome`, `cpf_cnpj`, `email`, `logradouro`, `nro`, `cep`, `bairro`, `cidade`, `estado`, `telefone`) VALUES(1, 'Aristides Peiter', '01097284085', 'adpeiter@hotmail.com', 'Av. Nereu Ramos', 1431, '89801800', 'Centro', 'Chapecó', 'SC', '4988396699');
+INSERT INTO `cliente` (`id_cliente`, `nome`, `cpf_cnpj`, `email`, `logradouro`, `nro`, `cep`, `bairro`, `cidade`, `estado`, `telefone`) VALUES(2, 'Cliente 1', '33333333333', 'email@email.com', 'rua 1', 3, '99999999', 'bairro 1', 'cidade 1', 'XX', '9977778888');
 
 -- --------------------------------------------------------
 
@@ -69,23 +53,26 @@ INSERT INTO `cliente` (`id_cliente`, `nome`, `cpf_cnpj`, `email`, `logradouro`, 
 -- Estrutura da tabela `estacionamento`
 --
 
-CREATE TABLE IF NOT EXISTS `estacionamento`.`estacionamento` (
-`id_estacionamento` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `estacionamento` (
+  `id_estacionamento` int(11) NOT NULL,
   `dh_entrada` varchar(100) NOT NULL COMMENT 'o custo em relação ao tempo',
   `dh_saida` varchar(45) DEFAULT NULL,
-  `nro_vaga` varchar(45) DEFAULT NULL,
   `vaga_id_vaga` int(11) NOT NULL,
   `cliente_id_cliente` int(11) NOT NULL,
+  `plano_id_plano_contratado` int(11) NOT NULL,
   `status` int(11) DEFAULT NULL,
   `token` char(6) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
 
 --
 -- Extraindo dados da tabela `estacionamento`
 --
 
-INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `nro_vaga`, `vaga_id_vaga`, `cliente_id_cliente`, `status`, `token`) VALUES
-(1, '09/05/2015 às 19h', '09/05/2015 às 20h', '00001', 1, 1, 1, '1', '1');
+INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `vaga_id_vaga`, `cliente_id_cliente`, `plano_id_plano_contratado`, `status`, `token`) VALUES(1, '2015-01-01T00:00', '2015-01-01T01:00', 1, 2, 1, 1, '913078');
+INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `vaga_id_vaga`, `cliente_id_cliente`, `plano_id_plano_contratado`, `status`, `token`) VALUES(8, '2015-12-02T23:00', '2015-12-03T05:00', 1, 2, 1, 1, '975605');
+INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `vaga_id_vaga`, `cliente_id_cliente`, `plano_id_plano_contratado`, `status`, `token`) VALUES(15, '2015-12-01T00:00', '2015-12-01T03:00', 5, 2, 1, 3, '698410');
+INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `vaga_id_vaga`, `cliente_id_cliente`, `plano_id_plano_contratado`, `status`, `token`) VALUES(18, '2015-06-02T20:00', '2015-06-02T22:00', 1, 2, 1, 0, '988711');
+INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `vaga_id_vaga`, `cliente_id_cliente`, `plano_id_plano_contratado`, `status`, `token`) VALUES(19, '2015-01-31T23:00', '2015-01-31T23:59', 1, 2, 1, 3, '287811');
 
 -- --------------------------------------------------------
 
@@ -93,12 +80,12 @@ INSERT INTO `estacionamento` (`id_estacionamento`, `dh_entrada`, `dh_saida`, `nr
 -- Estrutura da tabela `mensalidade`
 --
 
-CREATE TABLE IF NOT EXISTS `estacionamento`.`mensalidade` (
-`id_mensalidade` int(11) NOT NULL,
-  `ano` varchar(100) NOT NULL COMMENT 'o custo em relação ao tempo',
-  `mes` varchar(45) DEFAULT NULL,
-  `val_plano` double DEFAULT NULL,
-  `val_execed` double DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `mensalidade` (
+  `id_mensalidade` int(11) NOT NULL,
+  `ano` int(4) NOT NULL COMMENT 'o custo em relação ao tempo',
+  `mes` int(2) NOT NULL,
+  `val_plano` double NOT NULL,
+  `val_execed` double NOT NULL,
   `cliente_id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
 
@@ -106,8 +93,8 @@ CREATE TABLE IF NOT EXISTS `estacionamento`.`mensalidade` (
 -- Extraindo dados da tabela `mensalidade`
 --
 
-INSERT INTO `mensalidade` (`id_mensalidade`, `ano`, `mes`, `val_plano`, `val_execed`, `cliente_id_cliente`) VALUES
-(2, '2015', '06', 13.36, 2.36, 1);
+INSERT INTO `mensalidade` (`id_mensalidade`, `ano`, `mes`, `val_plano`, `val_execed`, `cliente_id_cliente`) VALUES(1, 2015, 6, 15, 2, 2);
+INSERT INTO `mensalidade` (`id_mensalidade`, `ano`, `mes`, `val_plano`, `val_execed`, `cliente_id_cliente`) VALUES(2, 2015, 5, 15, 0, 2);
 
 -- --------------------------------------------------------
 
@@ -115,22 +102,21 @@ INSERT INTO `mensalidade` (`id_mensalidade`, `ano`, `mes`, `val_plano`, `val_exe
 -- Estrutura da tabela `plano`
 --
 
-CREATE TABLE IF NOT EXISTS `estacionamento`.`plano` (
+CREATE TABLE IF NOT EXISTS `plano` (
   `id` int(11) NOT NULL,
-  `nome` varchar(20) NOT NULL,
+  `horas` int(3) NOT NULL,
   `valor` double NOT NULL COMMENT 'o custo em relação ao tempo',
-  `horas` int(11) NOT NULL COMMENT 'tempo em de estacionamento',
-  `valor_excedente` double DEFAULT NULL,
-  `descr` varchar(45) DEFAULT NULL
-  
+  `valor_excedente` double NOT NULL,
+  `descr` varchar(45) DEFAULT NULL,
+  `observacao` varchar(45) DEFAULT NULL,
+  `nome` varchar(20) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
 
 --
 -- Extraindo dados da tabela `plano`
 --
 
-INSERT INTO `plano` (`id`, `nome`, `valor`, `horas`, `valor_excedente`, `descr`) VALUES
-(1, 'Plano1', 20.0, 3, 0.5,'plano 1');
+INSERT INTO `plano` (`id`, `horas`, `valor`, `valor_excedente`, `descr`, `observacao`, `nome`) VALUES(1, 100, 100, 1.2, '', '', 'plano 1');
 
 -- --------------------------------------------------------
 
@@ -138,8 +124,8 @@ INSERT INTO `plano` (`id`, `nome`, `valor`, `horas`, `valor_excedente`, `descr`)
 -- Estrutura da tabela `plano_contratado`
 --
 
-CREATE TABLE IF NOT EXISTS `estacionamento`.`plano_contratado` (
-  `id_cliente` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `plano_contratado` (
+  `id_plano_contratado` int(11) NOT NULL,
   `cliente_id_cliente` int(11) NOT NULL,
   `plano_id_plano` int(11) NOT NULL,
   `data_contrato` date DEFAULT NULL,
@@ -150,8 +136,7 @@ CREATE TABLE IF NOT EXISTS `estacionamento`.`plano_contratado` (
 -- Extraindo dados da tabela `plano_contratado`
 --
 
-INSERT INTO `plano_contratado` (`id_cliente`, `cliente_id_cliente`, `plano_id_plano`, `data_contrato`, `observacao`) VALUES
-(0, 1, 1, '2015-06-16', NULL);
+INSERT INTO `plano_contratado` (`id_plano_contratado`, `cliente_id_cliente`, `plano_id_plano`, `data_contrato`, `observacao`) VALUES(1, 2, 1, '2015-06-25', '');
 
 -- --------------------------------------------------------
 
@@ -159,10 +144,10 @@ INSERT INTO `plano_contratado` (`id_cliente`, `cliente_id_cliente`, `plano_id_pl
 -- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `estacionamento`.`usuario` (
+CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL,
-  `senha` varchar(45) DEFAULT NOT NULL,
-  `login` varchar(45) DEFAULT NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
   `cliente_id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
@@ -170,9 +155,8 @@ CREATE TABLE IF NOT EXISTS `estacionamento`.`usuario` (
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `senha`, `login`, `cliente_id_cliente`) VALUES
-(1, 'ok', 'admin', 1),
-(2, 'ok', 'cliente', 1);
+INSERT INTO `usuario` (`id`, `senha`, `login`, `cliente_id_cliente`) VALUES(1, '123', 'admin', 1);
+INSERT INTO `usuario` (`id`, `senha`, `login`, `cliente_id_cliente`) VALUES(2, '123', 'cli1', 2);
 
 -- --------------------------------------------------------
 
@@ -180,20 +164,23 @@ INSERT INTO `usuario` (`id`, `senha`, `login`, `cliente_id_cliente`) VALUES
 -- Estrutura da tabela `vaga`
 --
 
-
-CREATE TABLE IF NOT EXISTS `estacionamento`.`vaga` (
-`id_vaga` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `vaga` (
+  `id_vaga` int(11) NOT NULL,
   `nro_vaga` varchar(50) NOT NULL COMMENT 'tempo em de estacionamento',
   `descricao` varchar(100) NOT NULL COMMENT 'o custo em relação ao tempo',
   `tipo` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='Table storing all customers. Holds foreign keys to the addre';
 
 --
 -- Extraindo dados da tabela `vaga`
 --
 
-INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES
-(1, '00001', 'Vaga para motos', 'Carro');
+INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES(1, '1', '', '1');
+INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES(2, '2', '', '1');
+INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES(3, '3', '', '1');
+INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES(4, '4', '', '1');
+INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES(5, '5', '', '1');
+INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES(6, '6', '', '1');
 
 --
 -- Indexes for dumped tables
@@ -203,43 +190,43 @@ INSERT INTO `vaga` (`id_vaga`, `nro_vaga`, `descricao`, `tipo`) VALUES
 -- Indexes for table `cliente`
 --
 ALTER TABLE `cliente`
- ADD PRIMARY KEY (`id_cliente`);
+  ADD PRIMARY KEY (`id_cliente`);
 
 --
 -- Indexes for table `estacionamento`
 --
 ALTER TABLE `estacionamento`
- ADD PRIMARY KEY (`id_estacionamento`), ADD KEY `fk_estacionamento_vaga1_idx` (`vaga_id_vaga`), ADD KEY `fk_estacionamento_cliente1_idx` (`cliente_id_cliente`);
+  ADD PRIMARY KEY (`id_estacionamento`,`vaga_id_vaga`,`cliente_id_cliente`), ADD UNIQUE KEY `token_UNIQUE` (`token`), ADD KEY `fk_estacionamento_vaga1_idx` (`vaga_id_vaga`), ADD KEY `fk_estacionamento_cliente1_idx` (`cliente_id_cliente`);
 
 --
 -- Indexes for table `mensalidade`
 --
 ALTER TABLE `mensalidade`
- ADD PRIMARY KEY (`id_mensalidade`,`cliente_id_cliente`), ADD KEY `fk_mensalidade_cliente1_idx` (`cliente_id_cliente`);
+  ADD PRIMARY KEY (`id_mensalidade`,`cliente_id_cliente`), ADD KEY `fk_mensalidade_cliente1_idx` (`cliente_id_cliente`);
 
 --
 -- Indexes for table `plano`
 --
 ALTER TABLE `plano`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `plano_contratado`
 --
 ALTER TABLE `plano_contratado`
- ADD PRIMARY KEY (`id_cliente`,`cliente_id_cliente`,`plano_id_plano`), ADD KEY `fk_plano_contratado_cliente1_idx` (`cliente_id_cliente`), ADD KEY `fk_plano_contratado_plano1_idx` (`plano_id_plano`);
+  ADD PRIMARY KEY (`id_plano_contratado`,`cliente_id_cliente`,`plano_id_plano`), ADD KEY `fk_plano_contratado_cliente1_idx` (`cliente_id_cliente`), ADD KEY `fk_plano_contratado_plano1_idx` (`plano_id_plano`);
 
 --
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
- ADD PRIMARY KEY (`id`,`cliente_id_cliente`), ADD KEY `fk_usuario_cliente1_idx` (`cliente_id_cliente`);
+  ADD PRIMARY KEY (`id`,`cliente_id_cliente`), ADD UNIQUE KEY `login` (`login`), ADD KEY `fk_usuario_cliente1_idx` (`cliente_id_cliente`);
 
 --
 -- Indexes for table `vaga`
 --
 ALTER TABLE `vaga`
- ADD PRIMARY KEY (`id_vaga`);
+  ADD PRIMARY KEY (`id_vaga`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -249,32 +236,32 @@ ALTER TABLE `vaga`
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `estacionamento`
 --
 ALTER TABLE `estacionamento`
-MODIFY `id_estacionamento` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_estacionamento` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `mensalidade`
 --
 ALTER TABLE `mensalidade`
-MODIFY `id_mensalidade` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id_mensalidade` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `plano`
 --
 ALTER TABLE `plano`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `vaga`
 --
 ALTER TABLE `vaga`
-MODIFY `id_vaga` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_vaga` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
