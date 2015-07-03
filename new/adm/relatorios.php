db03cf960a133338bf3636f5484d648123768ea5
jacsonmatte <jacsonmatte@gmail.com>
2015-07-03 15:58:03 -0300

    Relatórios ADM
    Terminados todos os relatórios do administrador
    Jacson e Patrick

--- new/adm/relatorios.php
+++ new/adm/relatorios.php
@@ -1,6 +1,10 @@
 <?php
 	require '../require/adm-aut.php';
+	require('../bd/relatoriosAdm.php');
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -10,120 +14,344 @@
 			require '../require/meta.html';
 			require '../require/js-base.html';
 		?>
+		<script>
+		//oculta os campos desnecessários para cada uma das opções
+		function desabilitaPlanos(opcao){
+				if (opcao.value == "planos") {
+					document.getElementById('dteDataInicial').disabled = true;
+					document.getElementById('dteDataFinal').disabled = true;
+					document.getElementById('txtIdentificacaoCliente').disabled = true;
+					document.getElementById('txtVaga').disabled = true;
+				}
+				document.getElementById('sltPlanos').disabled = false;
+
+			}
+			
+			function desabilitaClientes(opcao){
+				if (opcao.value == "clientes") {
+					document.getElementById('dteDataInicial').disabled = true;
+					document.getElementById('dteDataFinal').disabled = true;
+					document.getElementById('sltPlanos').disabled = true;
+					document.getElementById('txtVaga').disabled = true;
+				}
+				document.getElementById('txtIdentificacaoCliente').disabled = false;
+
+			}
+			
+			function habilitaTudo(opcao){
+				if (opcao.value == "planosClientes") {
+					document.getElementById('dteDataInicial').disabled = false;
+					document.getElementById('dteDataFinal').disabled = false;
+					document.getElementById('sltPlanos').disabled = false;
+					document.getElementById('txtVaga').disabled = false;
+					document.getElementById('txtIdentificacaoCliente').disabled = false;
+				}
+
+			}
+			
+			function desabilitaUtilizadasUsoVagasCadastradas(opcao){
+				if (opcao.value == "reservasUtilizadas" || opcao.value == "relatorioUsoVaga" || opcao.value == "reservasCadastradas") {
+					document.getElementById('dteDataInicial').disabled = true;
+					document.getElementById('dteDataFinal').disabled = true;
+					document.getElementById('sltPlanos').disabled = true;
+				}
+				
+				
+				document.getElementById('txtVaga').disabled = false;
+				document.getElementById('txtIdentificacaoCliente').disabled = false;
+
+			}
+			
+		
+		</script>
 	</head>
 	<?php
 		require '../require/menu-1.html';
-		$_SESSION['username'] = 'teste';
 		require '../require/menu-adm.html';
 		require '../require/menu-2-content-1.html';
 	?> 
 	<h3>Relatórios</h3>
-	<form role='form' class='text-center'>
+	<form role='form' class='text-center' method="POST" action="relatorios.php" name="relatorios">
 		<div class='form-group col-sm-3 text-left'>
 			<div class='col-sm-12 text-left form-group'>
-				<input type='radio' name='relatorio' id='rdbRelatorioPlanos' /><label for='rdbRelatorioPlanos'>&nbsp;Planos</label>
+				<input type='radio' name='relatorio' id='rdbRelatorioPlanos' value='planos' onclick="desabilitaPlanos(relatorios.relatorio);" /><label for='rdbRelatorioPlanos'>&nbsp;Planos</label>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
-				<input type='radio' name='relatorio' id='rdbRelatorioClientes' /><label for='rdbRelatorioClientes'>&nbsp;Clientes</label>
+				<input type='radio' name='relatorio' id='rdbRelatorioClientes' value='clientes' onclick="desabilitaClientes(relatorios.relatorio);"/><label for='rdbRelatorioClientes'>&nbsp;Clientes</label>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
-				<input type='radio' name='relatorio' id='rdbRelatorioPlanosClientes' /><label for='rdbRelatorioPlanosClientes'>&nbsp;Planos x Clientes</label>
+				<input type='radio' name='relatorio' id='rdbRelatorioPlanosClientes' value='planosClientes' onclick="habilitaTudo(relatorios.relatorio);"/><label for='rdbRelatorioPlanosClientes'>&nbsp;Planos x Clientes</label>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
-				<input type='radio' name='relatorio' id='rdbRelatorioReservasUtilizadas' /><label for='rdbRelatorioReservasUtilizadas'>&nbsp;Reservas utilizadas</label>
+				<input type='radio' name='relatorio' id='rdbRelatorioReservasUtilizadas' value='reservasUtilizadas' onclick="desabilitaUtilizadasUsoVagasCadastradas(relatorios.relatorio);"/><label for='rdbRelatorioReservasUtilizadas'>&nbsp;Reservas utilizadas</label>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
-				<input type='radio' name='relatorio' id='rdbRelatorioReservasCadastradas' /><label for='rdbRelatorioReservasCadastradas'>&nbsp;Reservas cadastradas</label>
+				<input type='radio' name='relatorio' id='rdbRelatorioReservasCadastradas' value='reservasCadastradas' onclick="desabilitaUtilizadasUsoVagasCadastradas(relatorios.relatorio);"/><label for='rdbRelatorioReservasCadastradas'>&nbsp;Reservas cadastradas</label>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
-				<input type='radio' name='relatorio' id='rdbRelatorioUsoVagas' /><label for='rdbRelatorioUsoVagas'>&nbsp;Uso de vagas</label>
+				<input type='radio' name='relatorio' id='rdbRelatorioUsoVagas' value='relatorioUsoVaga' onclick="desabilitaUtilizadasUsoVagasCadastradas(relatorios.relatorio);"/><label for='rdbRelatorioUsoVagas'>&nbsp;Uso de vagas</label>
 			</div>
 		</div>
 		<div class='form-group col-sm-9 text-left'>
 			<div class='col-sm-12 text-left form-group'>
 				<div class='col-sm-4'>
-					<input class='form-control' type='date' id='dteDataInicial' placeholder='Data Inicial'/>
+					<input class='form-control' type='date' name='dataInicial' id='dteDataInicial' placeholder='Data Inicial'/>
 				</div>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
 				<div class='col-sm-4'>
-					<input class='form-control' type='date' id='dteDataFinal' placeholder='Data Final'/>
+					<input class='form-control' type='date' name='dataFinal' id='dteDataFinal' placeholder='Data Final'/>
 				</div>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
 				<div class='col-sm-6'>
-					<input class='form-control' type='text' id='txtIdentificacaoCliente' placeholder='Identificação do cliente'/>
+					<input class='form-control' type='text' name='idCliente' id='txtIdentificacaoCliente' placeholder='Identificação do cliente'/>
 				</div>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
 				<div class='col-sm-3'>
-					<input class='form-control' type='text' id='txtVaga' placeholder='Vaga'/>
+					<input class='form-control' type='text' name='vaga' id='txtVaga' placeholder='Vaga'/>
 				</div>
 			</div>
 			<div class='col-sm-12 text-left form-group'>
 				<div class='col-sm-6'>
-					<select class='form-control' id='sltPlanos'>
-						<option>Plano 1</option>
-						<option>Plano 2</option>
-						<option>Plano 3</option>
+					<select class='form-control' name='planos' id='sltPlanos'>
+						<option value="">Selecione um Plano:</option>
+						<?php 
+							$dados = recPlano();
+							if(count($dados) > 0){
+								for($i = 0; $i < count($dados)-1; $i++){
+									$id = $dados["value"][$i];
+									echo '<option value='.$id.'>'.$dados[$i].'</option>';
+								}
+							}
+					
+						?>	
 					</select>
 				</div>
 			</div>
 		</div>
+
 		<div class='form-group col-sm-12 text-right'>
 			<span id='spnErroGerarRelatorio' class='help-inline'></span>&nbsp;
-			<input type='button' id='btnGerar' class='btn cmd-item' value='Gerar' />
+			<input type='submit' id='btnGerar' class='btn cmd-item' value='Gerar' />
 		</div>
 	</form>
-	<!-- Modal do relatório -->
-	<div class="modal fade" id="divRelatorio" role="dialog">
-		<div class="modal-dialog">		
-		<!-- Modal content-->
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title" id='h4TituloRelatorio' >Relatório exemplo</h4>
-				</div>
-				<div class="modal-body" id='divConteudoRelatorio'>
-					Aqui colocamos a table gerada dinamicamente
-				</div>
-		  </div>
-		</div>
-	</div>
+	<?php 
+		if (isset($_POST["relatorio"])) { 
+			?>
+			<link href="https://cdn.datatables.net/1.10.7/css/jquery.dataTables.css" rel="stylesheet" type="text/css"/>
+			<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+			<script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+			<table id="example" class="display" cellspacing="0" width="100%">
+			<?php
+			//imprime em formato de tabela todos os relatórios conforme selecionado pelo usuário
+			if ($_POST["relatorio"] == "planos") {
+				?>
+				<thead>
+					<tr>
+						<th>Plano</th>
+						<th>Valor</th>
+						<th>Horas</th>
+						<th>Valor Hora Excedente</th>
+						<th>Descrição</th>
+					</tr>
+				</thead>
+				
+				<tfoot>
+					<tr>
+						<th>Plano</th>
+						<th>Valor</th>
+						<th>Horas</th>
+						<th>Valor Hora Excedente</th>
+						<th>Descrição</th>
+					</tr>
+				</tfoot>
+				
+				<tbody>
+
+					<?php 
+					
+					echo buscaPlanos($_POST["planos"]); ?>
+				</tbody>
+			<?php
+			} else if ($_POST["relatorio"] == "clientes") {
+				?>
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>CPF/CNPJ</th>
+						<th>E-mail</th>
+						<th>Endereço</th>
+						<th>CEP</th>
+						<th>Telefone</th>
+					</tr>
+				</thead>
+				
+				<tfoot>
+					<tr>
+						<th>Nome</th>
+						<th>CPF/CNPJ</th>
+						<th>E-mail</th>
+						<th>Endereço</th>
+						<th>CEP</th>
+						<th>Telefone</th>
+					</tr>
+				</tfoot>
+				
+				<tbody>
+					<?php 
+					$busca = buscaClientes($_POST["idCliente"]);
+
+					if($busca) echo $busca; ?>
+				</tbody>
+			<?php		
+			} else if ($_POST["relatorio"] == "planosClientes") {
+				?>
+				<thead>
+					<tr>
+						<th>Cliente</th>
+						<th>Plano</th>
+						<th>Horas</th>
+						<th>Observação</th>
+					</tr>
+				</thead>
+				
+				<tfoot>
+					<tr>
+						<th>Cliente</th>
+						<th>Plano</th>
+						<th>Horas</th>
+						<th>Observação</th>
+					</tr>
+				</tfoot>
+				
+				<tbody>
+					<?php 
+					$busca = buscaPlanosClientes($_POST["dataInicial"],$_POST["dataFinal"], $_POST["idCliente"], $_POST["vaga"], $_POST["planos"]); 
+
+					if ($busca) echo $busca; ?>
+				</tbody>
+			<?php		
+			} else if ($_POST["relatorio"] == "reservasUtilizadas") {
+				?>
+				<thead>
+					<tr>
+						<th>Código</th>
+						<th>Data e hora de Entrada</th>
+						<th>Data e hora de Saída</th>
+						<th>Número da vaga</th>
+						<th>Token</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				
+				<tfoot>
+					<tr>
+						<th>Código</th>
+						<th>Data e hora de Entrada</th>
+						<th>Data e hora de Saída</th>
+						<th>Número da vaga</th>
+						<th>Token</th>
+						<th>Status</th>
+					</tr>
+				</tfoot>
+				
+				<tbody>
+					<?php 
+					$busca = buscaReservasUtilizadas($_POST["idCliente"], $_POST["vaga"], $_POST["relatorio"] ); 
+					if ($busca) echo $busca;?>				
+				</tbody>
+			<?php
+			} else if ($_POST["relatorio"] == "reservasCadastradas") {
+				?>
+				<thead>
+					<tr>
+						<th>Cliente</th>
+						<th>Nº da Vaga</th>
+						<th>Data Entrada</th>
+						<th>Data Saída</th>
+					</tr>
+				</thead>
+				
+				<tfoot>
+					<tr>
+						<th>Cliente</th>
+						<th>Nº da Vaga</th>
+						<th>Data Entrada</th>
+						<th>Data Saída</th>
+					</tr>
+				</tfoot>
+				
+				<tbody>
+				<?php 
+					$busca = buscaReservasUtilizadas($_POST["idCliente"], $_POST["vaga"], $_POST["relatorio"]); 
+					if ($busca) echo $busca;?>
+				</tbody>
+			<?php
+			} else if ($_POST["relatorio"] == "relatorioUsoVaga") {
+				?>
+				<thead>
+					<tr>
+						<th>Nº da Vaga</th>
+						<th>Tipo da Vaga</th>
+						<th>Data Entrada</th>
+						<th>Data Saída</th>
+						<th>Cliente</th>
+					</tr>
+				</thead>
+				
+				<tfoot>
+					<tr>
+						<th>Nº da Vaga</th>
+						<th>Tipo da Vaga</th>
+						<th>Data Entrada</th>
+						<th>Data Saída</th>
+						<th>Cliente</th>
+					</tr>
+				</tfoot>
+				
+				<tbody>
+					<?php 
+					$busca = buscaUsoDeVagas($_POST["idCliente"], $_POST["vaga"]);
+					if ($busca) echo $busca;  ?>
+				</tbody>
+			<?php
+			}
+		?>
+		</table>
 	<?php
+		}
 		require '../require/content-2-footer.html';
 	?>
 	
 	<script type='text/javascript'>
-	
-		$("#btnGerar").click(function() {
-			
-			$("#spnErroGerarRelatorio").text('');
-			
-			// obter os radio checados para ver se foi escolhido algum tipo de relatório
-			var opt = $('input[name=relatorio]:checked');
-			
-			if (opt.length == 1) {
-				
-				// verificar o tipo de relatório selecionado e os filtros necessários
-				
-				// se tiver tudo ok, chama o relatório por ajax
-				// recebe o retorno json
-				// chama a função que monta a tabela com o objeto json (ainda será criada)
-				// coloca a tabela na divConteudoRelatorio 
-				//$("#divConteudoRelatorio").empty();
-				//$("#divConteudoRelatorio").append(conteudo);
-				// exibe o modal
-				$("#divRelatorio").modal();
-			}
-			else
-				$("#spnErroGerarRelatorio").text('É necessário escolher um tipo de relatório');
-			
-		});
-		
-	
+	//exibe a tabela no formato data Table e traduz para pt-br
+		$(document).ready(function() {
+			$('#example').DataTable({
+				language: {
+					processing:     "Processando...",
+					search:         "Buscar:",
+					lengthMenu:     "Exibir _MENU_ itens por página",
+					info:           "Mostrando _START_ a _END_ de _TOTAL_ itens",
+					infoEmpty:      "Nenhum registro",
+					infoFiltered:   "",
+					infoPostFix:    "",
+					loadingRecords: "Carregando...",
+					zeroRecords:    "Nenhum registro",
+					emptyTable:     "Nenhum registro",
+					paginate: {
+						first:      "<<",
+						previous:   "<",
+						next:       ">",
+						last:       ">>"
+					},
+					aria: {
+						sortAscending:  ": ordem crescente",
+						sortDescending: ": ordem decrescente"
+					}
+				}
+			});
+		} );
 	</script>
-	
-	
-	
 </html>
---
+++ new/bd/relatoriosAdm.php
@@ -0,0 +1,151 @@
+<?php	
+	require('conectBd.php');
+	//busca todos os planos
+	function recPlano ()
+	{
+		$sql = "SELECT descr, id FROM `plano` WHERE 1";
+		$con = dbConnect("localhost","root","");
+		$result = dbConsulta($sql,"estacionamento",$con);
+		if(mysql_num_rows($result) == 0) return 0;
+ 		$dados = array();
+		while($row=mysql_fetch_array($result)){
+			$dados[] = $row["descr"];
+			$dados["value"][] = $row["id"];
+		}
+
+		return $dados;
+	}
+	//filtra os planos
+	function buscaPlanos ($planos) 
+	{
+		$sql = "SELECT  nome, valor, horas, valor_excedente, descr FROM plano";
+		
+		if ($planos != "") {
+			$sql .= " WHERE id = '{$planos}' ";
+		}
+
+		$con = dbConnect("localhost","root","");
+		$result = dbConsulta($sql,"estacionamento",$con);
+		
+		$dados = array();
+		
+		if (mysql_num_rows($result)) {
+			$dados = "";
+			while ($row=mysql_fetch_array($result)) {
+				$dados .= "<tr><td>".$row['nome']."</td><td>".$row['valor']."</td><td>".$row['horas']."</td><td>".$row['valor_excedente']."</td><td>".$row['descr']."</td></tr>";
+			}
+		}
+		
+		return $dados;
+	}
+	//filtra os clientes
+	function buscaClientes ($idCliente)
+	{
+		$sql = "SELECT nome, cpf_cnpj, email, logradouro, nro, cep, bairro, cidade, estado, telefone FROM cliente";
+
+		if ($idCliente != "")
+			$sql .= " WHERE id_cliente = $idCliente";
+
+
+		$con = dbConnect("localhost","root","");
+		$result = dbConsulta($sql,"estacionamento",$con);
+		
+		$dados = array();
+		
+		if (mysql_num_rows($result)) {
+			$dados = "";
+			while ($row=mysql_fetch_array($result)) {
+				$dados .= "<tr><td>".$row['nome']."</td><td>".$row['cpf_cnpj']."</td><td>".$row['email']."</td><td>".$row['logradouro']."&nbsp-&nbsp".$row['bairro']."&nbsp-&nbsp".$row['cidade']."&nbsp-&nbsp".$row['estado']."</td><td>".$row['cep']."</td><td>".$row['telefone']."</td></tr>";
+			}
+		} else return 0;
+		
+		return $dados;
+	}
+	//filtra os planos dos clientes
+	function buscaPlanosClientes ($dataInicial, $dataFinal, $idCliente, $planos) 
+	{
+		$sql = "SELECT cliente.nome as nome, plano.nome as planoNome, plano.horas as horas, plano_contratado.observacao as obs FROM plano_contratado, plano, cliente WHERE cliente.id_cliente = plano_contratado.cliente_id_cliente AND plano_contratado.plano_id_plano = plano.id";
+
+		if ($idCliente > 0)
+			$sql .= " AND cliente_id_cliente = '$idCliente'";
+		
+		if ($dataInicial != '' && $dataFinal != '') {
+			$sql .= " AND data_contrato >= '$dataInicial' AND data_contrato <= '$dataFinal'";
+		} else if ($dataInicial != '' && $dataFinal == '') {
+			$sql .= " AND data_contrato >= '$dataInicial'";
+		} else if ($dataInicial == '' && $dataFinal != '') {
+			$sql .= " AND data_contrato <= '$dataFinal'";
+		}
+
+		if ($planos != "") {
+			$sql .= " AND plano_id_plano = '$planos'";
+		}
+
+		$con = dbConnect("localhost","root","");
+		$result = dbConsulta($sql,"estacionamento",$con);
+		
+		$dados = array();
+		
+		if (mysql_num_rows($result)) {
+			$dados = "";
+			while ($row=mysql_fetch_array($result)) {
+				$dados .= "<tr><td>".$row['nome']."</td><td>".$row['planoNome']."</td><td>".$row['horas']."</td><td>".$row['obs']."</td></tr>";
+			}
+		} else return 0;
+		
+		return $dados;
+	}
+//filtra as reservas utilizadas
+	function buscaReservasUtilizadas ($idCliente, $vaga, $relatorio)
+	{
+
+		$sql = "SELECT e.id_estacionamento codigo, e.dh_entrada entrada, e.dh_saida saida, v.nro_vaga vaga, e.token token, e.status status FROM estacionamento e, vaga v WHERE e.vaga_id_vaga = v.id_vaga ";
+		
+		if($relatorio == "reservasUtilizadas"){
+			$sql .= "AND e.status = '1'";
+		}
+		
+		if($idCliente != ''){
+			$sql .= " AND e.cliente_id_cliente = '$idCliente' ";
+		}
+		
+		if($vaga != ''){
+			$sql .= " AND e.vaga_id_vaga = '$vaga' ";
+		}
+		
+		$con = dbConnect("localhost","root","");
+		$result = dbConsulta($sql,"estacionamento",$con);
+
+		$dados = '';
+		if (mysql_num_rows($result)) {
+			$dados = "";
+			while ($row = mysql_fetch_array($result)) {
+					
+					$dados .= "<tr><td>".$row['codigo']."</td><td>".$row['entrada']."</td><td>".$row['saida']."</td><td>".$row['vaga']."</td><td>".$row['token']."</td><td>".$row['status']."</td></tr>";
+			}
+		} else return 0;
+		return $dados;
+	}
+	//filtra as vagas
+	function buscaUsoDeVagas ($idCliente, $vaga)
+	{
+		$sql = "SELECT vaga.nro_vaga as vaga, vaga.tipo as tipo, dh_entrada, dh_saida, cliente.nome as nome FROM estacionamento, cliente, vaga WHERE estacionamento.vaga_id_vaga = vaga.id_vaga AND estacionamento.cliente_id_cliente = cliente.id_cliente";
+		if ($idCliente != "")
+			$sql .= " AND estacionamento.cliente_id_cliente = $idCliente";
+		if ($vaga)
+			$sql .= " AND estacionamento.vaga_id_vaga = $vaga";
+			
+		$con = dbConnect("localhost","root","");
+		$result = dbConsulta($sql,"estacionamento",$con);
+
+		$dados = array();
+		if (mysql_num_rows($result)) {
+			$dados = "";
+			while ($row = mysql_fetch_array($result)) {
+				$dados .= "<tr><td>".$row['vaga']."</td><td>".$row['tipo']."</td><td>".$row['dh_entrada']."</td><td>".$row['dh_saida']."</td><td>".$row['nome']."</td></tr>";
+			}
+		} else return 0;	
+				
+		return $dados;
+	}
+?>
