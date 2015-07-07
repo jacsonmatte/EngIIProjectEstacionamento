f4c7b5ede63d3d38fbe230e01ccb9da78890ec46
Aristides D. Peiter <aristosdp@gmail.com>
2015-07-06 22:19:32 -0300

    correções cliente/planos contratados
    modificada forma de busca (feito igual as demais páginas, com php)
    corrigidos filtros

--- new/bd/plano_contratado.php
+++ new/bd/plano_contratado.php
@@ -17,4 +17,33 @@ function buscarPlanosCliente($idCliente, $dataInicial, $dataFinal, $ordenar) {
 	
 }
 
+function buscarPlanosPorValorTempoSituacao($idCliente, $horasMin, $horasMax, $valorMin, $valorMax, $situacao) {
+
+	$sql = "SELECT p.nome nome, p.valor valor, p.horas horas, p.valor_excedente excedente, pc.data_contrato data, pc.id_plano_contratado id_plano_contratado, pc.status status FROM plano_contratado pc JOIN plano p ON p.id = pc.plano_id_plano WHERE cliente_id_cliente = $idCliente";
+	
+	if ($horasMin <> '')
+		$sql .= " AND p.horas >= '$horasMin'";
+	
+	if ($horasMax <> '')
+		$sql .= " AND p.horas <= '$horasMax'";
+	
+	if ($valorMin <> '')
+		$sql .= " AND p.valor >= '$valorMin'";
+	
+	if ($valorMax <> '')
+		$sql .= " AND p.valor <= '$valorMax'";
+	
+	if ($situacao == 'ativos')
+		$sql .= " AND pc.status = 0";
+	
+	if ($situacao == 'inativos')
+		$sql .= " AND pc.status = 1";
+	
+	$con = dbConnect("localhost","root","");
+	$result = dbConsulta($sql,"estacionamento",$con);
+	
+	return $result;
+	
+}
+
 ?>
--- new/cliente/planos_contratados.php
+++ new/cliente/planos_contratados.php
@@ -1,12 +1,6 @@
 <?php 
 	require '../require/cliente-aut.php'; 
 	require '../bd/conectBd.php"';
-	
-	$nomecliente = $_SESSION['username'];
-	$sql="SELECT id_cliente FROM cliente INNER JOIN usuario ON cliente.id_cliente = usuario.cliente_id_cliente WHERE usuario.login='{$nomecliente}'";
-	$conexao = dbConnect("localhost","root","");
-	$query = dbConsulta($sql,"estacionamento", $conexao);
-	$id_cliente = mysql_fetch_array($query)
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +10,15 @@
 		<?php
 			require '../require/meta.html';
 			require '../require/js-base.html';
+			require '../bd/plano_contratado.php';
 		?>
+		<script type='text/javascript'>
+			function exibirPlanos(html) {
+				$("#divListaPlanosContratados").html(html);
+				$("#divRelatorio").modal();
+			}
+		</script>
+		
 	</head>
 	<?php
 		require '../require/menu-1.html';
@@ -24,119 +26,78 @@
 		require '../require/menu-2-content-1.html';
 	?> 
 	<h3>Planos contratados</h3>
-		<form role='form' class='text-center'>
-		<div class='form-group col-sm-3 text-left' title='Informe a quantidade mínima de horas dos planos que deseja pesquisar'>
-			<label for='txtPesquisaQuantidadeHorasMin'>Horas (mínimo):</label>
-			<input type='text' class='form-control' id='txtPesquisaQuantidadeHorasMin' />
-		</div>
-		<div class='form-group col-sm-3 text-left' title='Informe a quantidade máxima de horas dos planos que deseja pesquisar' >
-			<label for='txtPesquisaQuantidadeHorasMax'>Horas (máximo):</label>
-			<input type='text' class='form-control' id='txtPesquisaQuantidadeHorasMax'/>
-		</div>
-		<div class='form-group col-sm-3 text-left' title='Informe o valor mínimo dos planos que deseja pesquisar'>
-			<label for='txtPesquisaValorMin'>Valor (mínimo):</label>
-			<input type='text' class='form-control' id='txtPesquisaValorMin' />
-		</div>
-		<div class='form-group col-sm-3 text-left' title='Informe o valor máximo dos planos que deseja pesquisar' >
-			<label for='txtPesquisaValorMax'>Valor (máximo):</label>
-			<input type='text' class='form-control' id='txtPesquisaValorMax'/>
-		</div>
-		<div class='form-group col-sm-12 text-left'>
-			<input type='radio' id='rdbPesquisaSomentePlanosAtivos' name='pesquisaPlanoSituacao'/><label for='rdbPesquisaSomentePlanosAtivos'>&nbsp;Apenas planos ativos</label>&nbsp;
-			<input type='radio' id='rdbPesquisaSomentePlanosInativos' name='pesquisaPlanoSituacao'/><label for='rdbPesquisaSomentePlanosInativos'>&nbsp;Apenas planos inativos</label>&nbsp;
-			<input type='radio' id='rdbPesquisaTodosPlanos' name='pesquisaPlanoSituacao'/ checked><label for='rdbPesquisaTodosPlanos'>&nbsp;Todos</label>
-		</div>
-		<div class='form-group col-sm-12 text-right'>
-			<span id='spnErroPesquisarMensalidades'></span>
-			<input type='button' id='btnPesquisar' class='btn cmd-item' value='Pesquisar' />
-		</div>
-
-
-	</form>
-	<!-- Modal do relatório -->
-	<div class="modal fade" id="divRelatorio" role="dialog">
-		<div class="modal-dialog">		
-		<!-- Modal content-->
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title" id='h4TituloRelatorio' >Planos Contratados:</h4>
+		
+		<form role='form' class='text-center' action='planos_contratados.php' method='POST'>
+			<!-- Modal do relatório -->
+			<div class="modal fade" id="divRelatorio" role="dialog">
+				<div class="modal-dialog">	
+				<!-- Modal content-->
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title" id='h4TituloRelatorio' >Planos Contratados:</h4>
+						</div>
+						<div class="modal-body" id='divListaPlanosContratados'>
+							
+						</div>
+				  </div>
 				</div>
-				<div class="modal-body" id='divConteudoRelatorio'>
-					<p id="conteudoRelatorio" ></p>
-				</div>
-		  </div>
-		</div>
-	</div>
+			</div>
+			<div class='form-group col-sm-3 text-left' title='Informe a quantidade mínima de horas dos planos que deseja pesquisar'>
+				<label for='txtPesquisaQuantidadeHorasMin'>Horas (mínimo):</label>
+				<input type='text' class='form-control' id='txtPesquisaQuantidadeHorasMin' name='horas_min' />
+			</div>
+			<div class='form-group col-sm-3 text-left' title='Informe a quantidade máxima de horas dos planos que deseja pesquisar' >
+				<label for='txtPesquisaQuantidadeHorasMax'>Horas (máximo):</label>
+				<input type='text' class='form-control' id='txtPesquisaQuantidadeHorasMax' name='horas_max' />
+			</div>
+			<div class='form-group col-sm-3 text-left' title='Informe o valor mínimo dos planos que deseja pesquisar'>
+				<label for='txtPesquisaValorMin'>Valor (mínimo):</label>
+				<input type='text' class='form-control' id='txtPesquisaValorMin' name='valor_min' />
+			</div>
+			<div class='form-group col-sm-3 text-left' title='Informe o valor máximo dos planos que deseja pesquisar' >
+				<label for='txtPesquisaValorMax'>Valor (máximo):</label>
+				<input type='text' class='form-control' id='txtPesquisaValorMax' name='valor_max' />
+			</div>
+			<div class='form-group col-sm-12 text-left'>
+				<input type='radio' id='rdbPesquisaSomentePlanosAtivos' name='pesquisaPlanoSituacao' value='ativos' /><label for='rdbPesquisaSomentePlanosAtivos'>&nbsp;Apenas planos ativos</label>&nbsp;
+				<input type='radio' id='rdbPesquisaSomentePlanosInativos' name='pesquisaPlanoSituacao' value='inativos' /><label for='rdbPesquisaSomentePlanosInativos'>&nbsp;Apenas planos inativos</label>&nbsp;
+				<input type='radio' id='rdbPesquisaTodosPlanos' name='pesquisaPlanoSituacao' value='todos' checked='checked'><label for='rdbPesquisaTodosPlanos'>&nbsp;Todos</label>
+			</div>
+			<div class='form-group col-sm-12 text-right'>
+				<span id='spnErroPesquisarMensalidades'></span>
+				<input type='submit' id='btnPesquisar' name='pesquisar' class='btn cmd-item' value='Pesquisar' />
+			</div>
+			<?php
+			
+				if (isset($_POST['pesquisar'])) {
+					$planosContratados = buscarPlanosPorValorTempoSituacao($_SESSION['id_cliente'], $_POST['horas_min'], $_POST['horas_max'], $_POST['valor_min'], $_POST['valor_max'], $_POST['pesquisaPlanoSituacao']);
+					$html = '';
+					if (!$planosContratados)
+						echo "<strong style='color: #FFF'>Oops! Parece que tivemos um problema...</strong>";
+					else if (mysql_num_rows($planosContratados) == 0)
+						echo "<strong>Nenhum plano encontrado</strong>";
+					else {
+								
+						$i = 0;
+						$html = "<table style='max-width: 95%' id='tblResultadoPesquisa'><thead><tr class='bg-all' style='color: #FFF'><th style='text-align: center; border-left: solid 1px #FFF;'>Código</th><th style='text-align: center;'>Nome</th><th style='text-align: center; border-left: solid 1px #FFF;'>Valor</th><th style='text-align: center; border-left: solid 1px #FFF;'>Horas</th><th style='text-align: center; border-left: solid 1px #FFF;'>Valor excedente</th><th style='text-align: center; border-left: solid 1px #FFF;'>Data</th><th style='text-align: center; border-left: solid 1px #FFF;'>Status</th></tr></thead><tbody>";
+							$status = '';	
+						while ($row = mysql_fetch_assoc($planosContratados)) {
+							$status = $row['status'] == 0 ? "ativo" : "inativo";
+							$html .= "<tr style='background: " . ($i % 2 == 0 ? "#CCC'" : "#FFF'") . "><td>" . $row['id_plano_contratado'] . "</td><td>" . $row['nome'] . "</td><td>" . $row['valor'] . "</td><td>" . $row['horas'] . "</td><td>" . $row['excedente'] . "</td><td>" . $row['data'] . "</td><td>" . $status . "</td></tr>"; $i++;
+						}
+						
+						$html .= "</tbody></table>";
+						echo "<script> exibirPlanos(\"" . $html . "\")</script>";
+						
+					}
+					
+				}
+			
+			?>
+		</form>
 	<?php
 		require '../require/content-2-footer.html';
 	?>
-	
-	<script type='text/javascript'>
-	
-		$("#btnPesquisar").click(function() {
-			
-			$("#spnErroGerarRelatorio").text('');
-			
-			// obter os radio checados para ver se foi escolhido algum tipo de relatório
-			var opt = $('input[name=pesquisaPlanoSituacao]:checked');
-			
-			if (opt.length == 1) {
-				var txtRelatorio = $('p[id=conteudoRelatorio]');
-				
-				$( txtRelatorio ).html('Carregando...');
-				
-				// verificar o tipo de relatório selecionado e os filtros necessários
-				
-				// se tiver tudo ok, chama o relatório por ajax
-				// recebe o retorno json
-				// chama a função que monta a tabela com o objeto json (ainda será criada)
-				// coloca a tabela na divConteudoRelatorio 			
-				
-				$.getJSON(
-					'function.php',
-					{ id_cliente: "<?php echo $id_cliente['id_cliente'];?>", 
-					horas_min: $('input[id=txtPesquisaQuantidadeHorasMin]').val(),
-					horas_max: $('input[id=txtPesquisaQuantidadeHorasMax]').val(),
-					valor_min: $('input[id=txtPesquisaValorMin]').val(),
-					valor_max: $('input[id=txtPesquisaValorMax]').val()},
-					function(json)
-					{
-						$( txtRelatorio ).html( json.txtValor );
-					}
-				);
-
-				// exibe o modal
-				$("#divRelatorio").modal();
-				$('#tblResultadoPesquisa').DataTable({
-					language: {
-						processing:     "Processando...",
-						search:         "Buscar:",
-						lengthMenu:     "Exibir _MENU_ itens por página",
-						info:           "Mostrando _START_ a _END_ de _TOTAL_ itens",
-						infoEmpty:      "Nenhum registro",
-						infoFiltered:   "",
-						infoPostFix:    "",
-						loadingRecords: "Carregando...",
-						zeroRecords:    "Nenhum registro",
-						emptyTable:     "Nenhum registro",
-						paginate: {
-							first:      "<<",
-							previous:   "<",
-							next:       ">",
-							last:       ">>"
-						},
-						aria: {
-							sortAscending:  ": ordem crescente",
-							sortDescending: ": ordem decrescente"
-						}
-					}
-				});
-
-			} 
-		});
-		
-	
-	</script>
 	
 </html>
